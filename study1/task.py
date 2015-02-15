05be5ea6f4bfecdfa4da9be0b3c31b74b37e51f2
tmknom <tmk.nom@gmail.com>
2015-02-16 02:07:31 +0900

    task.py作成

---
+++ study1/task.py
@@ -0,0 +1,15 @@
+# -*- encoding:utf-8 -*-
+
+from fabric.decorators import task
+
+@task
+def deploy():
+  task_one()
+  task_two()
+
+def task_one():
+  print("one!")
+
+def task_two():
+  print("two!!")
+
